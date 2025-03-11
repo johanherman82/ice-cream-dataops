6dbd03833fb1fb3b96b89c2b76064ddee0712162
johan.herman.storm@akerbp.com <johan.herman.storm@akerbp.com>
2025-03-11 10:32:55 -0500

    commit110325

--- ice-cream-dataops/modules/bootcamp/ice_cream_api/transformations/create_asset_hierarchy.sql
+++ ice-cream-dataops/modules/bootcamp/ice_cream_api/transformations/create_asset_hierarchy.sql
@@ -2,6 +2,6 @@ select
     cast(`external_id` as STRING) as externalId,
     cast(`name` as STRING) as name,
     cast(`description` as STRING) as description,
-    if(`parent_external_id`=='' OR `parent_external_id` == null, null, node_reference('icapi_dm_space', 'parent_external_id')) as parent
+    if(`parent_external_id`=='' OR `parent_external_id` == null, null, node_reference('icapi_dm_space', `parent_external_id`)) as parent
 from
     `ice-cream-factory-db`.`assets`;
